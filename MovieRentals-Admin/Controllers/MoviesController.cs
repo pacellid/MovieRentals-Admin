ff6043ea4e295e1c0aa4173769da32ffb6524395
Domonic Pacelli <pacellid@thomas.edu>
2018-11-20 15:41:53 -0500

    Completed Assignment5

--- MovieRentals-Admin/Controllers/MoviesController.cs
+++ MovieRentals-Admin/Controllers/MoviesController.cs
@@ -24,46 +24,11 @@ namespace Domonic.Controllers
             return View();
         }
 
-        [HttpGet]
         public ActionResult ToRent()
         {
-            //var model = new MovieModel
-            //{
-            //    AvailableMovies = AvailableMovies()
-            //};
+            ViewBag.MoviesToRent = db.Movies.ToList();
 
             return View();
-        }
-
-        [HttpPost]
-        public ActionResult ToRent(Movie m)
-        {
-           // if (ModelState.IsValid)
-           // {
-           //     return View("Rented", m.SelectedMovies);
-           // }
-           // m.AvailableMovies = AvailableMovies();
-
-            return View(m);
-        }
-
-
-        //generate the list of movies I am going to put up for rent
-        private IList<SelectListItem> AvailableMovies()
-        {
-            return new List<SelectListItem>
-            {
-            new SelectListItem { Value = "GhostBusters", Text = "GhostBusters" },
-            new SelectListItem { Value = "Raising Arizona", Text = "Raising Arizona" },
-            new SelectListItem { Value = "Charlie and The Chocolate Factory", Text = "Charlie and The Chocolate Factory" },
-            new SelectListItem { Value = "Batman Begins", Text = "Batman Begins" },
-            new SelectListItem { Value = "Apocalypse Now", Text = "Apocalypse Now" },
-            new SelectListItem { Value = "Back To The Future", Text = "Back To The Future" },
-            new SelectListItem { Value = "John Wick", Text = "John Wick" },
-            new SelectListItem { Value = "Star Wars: Return of The Jedi", Text = "Star Wars: Return of The Jedi" },
-            new SelectListItem { Value = "Avengers: Infinity War", Text = "Avengers: Infinity War" },
-            new SelectListItem { Value = "Bill and Ted's Excellent Adventure", Text = "Bill and Ted's Excellent Adventure" }
-        };
         }
     }
     
